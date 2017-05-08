8fbb9cde5b353143770dd2774b536b23e2cdd8f3
Felipe Armoni <felipe.armoni@gmail.com>
2017-05-08 10:24:34 -0300

    Added effect to change the nav-bar when the page is scrolled down.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,10 +10,24 @@ import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 export class NavBarComponent implements OnInit {
 
   public isCollapsed = true;
+  public navBarOn: boolean;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
+    this.navBarOn = false;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+
+    const number = this.document.body.scrollTop;
+    if (number > 100) {
+      this.navBarOn = true;
+    }
+    else if (this.navBarOn && number < 10) {
+      this.navBarOn = false;
+    }
   }
 
 }
